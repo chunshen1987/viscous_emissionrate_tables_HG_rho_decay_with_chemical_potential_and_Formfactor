f832da8af797b6f0c377f03828724753e288e7cf
Chun Shen <chunshen1987@gmail.com>
2014-11-08 11:17:32 -0500

    add the missing Table.cpp

--- Arsenal.h
+++ Arsenal.h
@@ -20,19 +20,30 @@ vector<double> stringToDoubles(string str);
 void
 gauss (int npts, int job, double a, double b, double xpts[], double weights[]); //generate points and weights for gaussian quadrature, code is in gauss.cpp
 
-double interpCubicDirect(vector<double>* x, vector<double>* y, double xx);
-double interpCubicMono(vector<double>* x, vector<double>* y, double xx);
-double interpLinearDirect(vector<double>* x, vector<double>* y, double xx);
-double interpLinearMono(vector<double>* x, vector<double>* y, double xx);
-double interpNearestDirect(vector<double>* x, vector<double>* y, double xx);
-double interpNearestMono(vector<double>* x, vector<double>* y, double xx);
+double interpCubicDirect(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+double interpCubicMono(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+double interpLinearDirect(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+double interpLinearMono(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+double interpNearestDirect(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+double interpNearestMono(vector<double>* x, vector<double>* y, double xx, bool allowExtrapolation=false);
+
+inline double interpCubic4Points(double A0, double A1, double A2, double A3, double dx, double xx)
+// Assuming that A0, A1, A2, A3 are the values located at 0, dx, 2*dx, and 3*dx, interpolate the value
+// at xx using cubic polynomials.
+{
+    double deltaX = xx - dx;
+    return (-A0+3.0*A1-3.0*A2+A3)/(6.0*dx*dx*dx)*deltaX*deltaX*deltaX
+            + (A0-2.0*A1+A2)/(2.0*dx*dx)*deltaX*deltaX
+            - (2.0*A0+3.0*A1-6.0*A2+A3)/(6.0*dx)*deltaX
+            + A1;
+}
 
 double invertFunc(double (*func)(double), double y, double xL, double xR, double dx, double x0, double relative_accuracy=1e-10);
 
 double invertTableDirect_hook(double xx);
 double invertTableDirect(vector<double>* x, vector<double>* y, double y0, double x0, double relative_accuracy=1e-10);
 
-long binarySearch(vector<double>* A, double value);
+long binarySearch(vector<double>* A, double value, bool skip_out_of_range=false);
 
 void outputFunctionerror(string function_name, string massage, double value, int level);
 
